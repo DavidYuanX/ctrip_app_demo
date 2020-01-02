4b0e7527a273ee753e4c283ae4a723f8059d817a
David <David.Yuan.X@gmail.com>
2020-01-02 22:45:12 +0800

    Http、future、Json、shared_preferences
    Http: 网络请求方法。
    future：接收网络请求 结果，并以类实例。
    Json： 解析 json 。
    shared_preferences：本地存储。

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,11 @@
+import 'dart:math';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
 const APPBAR_SCROLL_OFFSET = 100;
 
 class HomePage extends StatefulWidget {
@@ -15,17 +21,30 @@ class _HomePageState extends State<HomePage> {
     'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577892165738&di=d3b12c26a39d44dd0ce6536ec04c4abe&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2Fef6043c0f407dac052c3b53b533274e30feb8b3e810f-dzocHu_fw658',
   ];
   double appBarAlpha = 0;
+
   _onScroll(offset) {
-    double alpha = offset/APPBAR_SCROLL_OFFSET;
-    if(alpha<0){
+    double alpha = offset / APPBAR_SCROLL_OFFSET;
+    if (alpha < 0) {
       alpha = 0;
-    }else if(alpha>1){
-      alpha=1;
+    } else if (alpha > 1) {
+      alpha = 1;
     }
     setState(() {
       appBarAlpha = alpha;
     });
     print(appBarAlpha);
+  }
+
+  String showResult = '';
+  String countString = '';
+  String localCount = '';
+
+  Future<CommonModel> fetchPost() async {
+    final response = await http
+        .get('http://www.devio.org/io/flutter_app/json/test_common_model.json');
+    Utf8Decoder utf8decoder = Utf8Decoder();
+    final result = json.decode(utf8decoder.convert(response.bodyBytes));
+    return CommonModel.fromJson(result);
   }
 
   @override
@@ -59,15 +78,71 @@ class _HomePageState extends State<HomePage> {
                           pagination: SwiperPagination(),
                         ),
                       ),
-                      Container(
-                        height: 800,
-                        child: ListTile(
-                          title: Text('哈哈'),
-                        ),
-                      )
+                      InkWell(
+                          onTap: () {
+                            fetchPost().then((CommonModel value) {
+                              setState(() {
+                                showResult =
+                                    '请求结果：\nhideAppBar: ${value.hideAppBar}\nicon: ${value.icon}';
+                              });
+                            });
+                          },
+                          child: Text(
+                            '点我',
+                            style: TextStyle(fontSize: 26),
+                          )),
+                      Text(showResult),
+                      FutureBuilder<CommonModel>(
+                          future: fetchPost(),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<CommonModel> snapshot) {
+                            switch (snapshot.connectionState) {
+                              case ConnectionState.none:
+                                return Text('Input a Url to start');
+                              case ConnectionState.waiting:
+                                return Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              case ConnectionState.active:
+                                return new Text('');
+                              case ConnectionState.done:
+                                if (snapshot.hasError) {
+                                  return new Text(
+                                    '${snapshot.error}',
+                                    style: TextStyle(color: Colors.red),
+                                  );
+                                } else {
+                                  return new Column(
+                                    children: <Widget>[
+                                      Text('icon:${snapshot.data.icon}'),
+                                      Text(
+                                          'statusBarColor:${snapshot.data.statusBarColor}'),
+                                      Text('title:${snapshot.data.title}'),
+                                      Text('url:${snapshot.data.url}'),
+                                    ],
+                                  );
+                                }
+                            }
+                          }),
+                      RaisedButton(
+                        onPressed: _incrementCounter,
+                        child: Text('Increment Count'),
+                      ),
+                      RaisedButton(
+                        onPressed: _getCounter,
+                        child: Text('Get Count'),
+                      ),
+                      Text(
+                        countString,
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        localCount,
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text('111')
                     ],
-                  ))
-          ),
+                  ))),
           Opacity(
             opacity: appBarAlpha,
             child: Container(
@@ -80,9 +155,46 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
-          )
+          ),
+//          Text(showResult)
         ],
       ),
     );
+  }
+
+  _incrementCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      countString = countString + " 1";
+    });
+    int counter = (prefs.getInt('counter') ?? 0) + 1;
+    await prefs.setInt('counter', counter);
+  }
+
+  _getCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      localCount = prefs.getInt('counter').toString();
+    });
+  }
+}
+
+class CommonModel {
+  final String icon;
+  final String title;
+  final String url;
+  final String statusBarColor;
+  final bool hideAppBar;
+
+  CommonModel(
+      {this.icon, this.title, this.url, this.statusBarColor, this.hideAppBar});
+
+  factory CommonModel.fromJson(Map<String, dynamic> json) {
+    return CommonModel(
+        icon: json['icon'],
+        title: json['title'],
+        url: json['url'],
+        statusBarColor: json['statusBarColor'],
+        hideAppBar: json['hideAppBar']);
   }
 }
